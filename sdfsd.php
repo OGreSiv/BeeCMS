a08084adbc32b0247b3fc0ff587c0b42c7f9d289
OGreSiv <s.seroed@gmail.com>
2016-05-08 16:18:39 +0300

    test

---
+++ sdfsd.php
@@ -0,0 +1,11 @@
+<?php
+ /**
+ * sdfsd.php 
+ *
+ * @author BeeCMS team <s.seroed@gmail.com>
+ * @link http://bee-cms.net
+ * @copyright 2015 INTFOM
+ * @package bee-cms.local
+ * @since 0.1
+ * Date: 08.05.2016
+ */
